5ea1a88af1de2205ded0ee675185b34711fa55df
Hiroshi Hashiguchi <hashiguchi@lakesoft.jp>
2014-01-18 10:09:57 +0900

    Initial Commit

---
+++ LKCodingObject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LKCodingObject
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/18.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKCodingObject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LKCodingObject
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/18.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
